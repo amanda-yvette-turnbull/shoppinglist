099d11f1dce022fef38185174c5ee581a2cdcc3e
amanda <63027140+amanda-yvette-turnbull@users.noreply.github.com>
2023-05-29 18:55:40 +1200

    Update 20160802144759_recipes.js

--- server/db/migrations/20160802144759_recipes.js
+++ server/db/migrations/20160802144759_recipes.js
@@ -1,11 +1,10 @@
 exports.up = (knex) => {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('recipes', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.string('email')
   })
 }
 
 exports.down = (knex) => {
-  return knex.schema.dropTable('users')
+  return knex.schema.dropTable('recipes')
 }
