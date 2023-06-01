4250b8bad1b5f093fd6241698ac87fbdf449367b
Amanda <amandayvetteturnbull@gmail.com>
2023-06-01 14:34:46 +1200

    fixed id bug + working navbar

--- server/db/db.js
+++ server/db/db.js
@@ -5,8 +5,10 @@ const connection = require('knex')(config)
 module.exports = {
   getRecipes,
   getRecipe,
+  getIngredients,
 }
 
+//Recipies
 function getRecipes(db = connection) {
   return db('recipes').select()
 }
@@ -15,8 +17,15 @@ function getRecipe(id, db = connection) {
   return db('recipes').select().where('id', id).first()
 }
 
-// function getUser(id, db = connection) {
-//   return db('ingredients').where('id', id).first()
-// }
+//Ingredients
+function getIngredients(db = connection) {
+  return db('ingredients').select()
+}
 
+function getIngredient() {}
+
+//Shoppinglist
+function getAllRecipies() {}
+
+//db diagram
 //https://dbdiagram.io/d/647451e67764f72fcf02dd27
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,18 +2,27 @@ const express = require('express')
 const db = require('../db/db')
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+//recipe routes
+router.get('/recipes', async (req, res) => {
   const recipes = await db.getRecipes()
-  res.render('home', { recipes: recipes })
+  res.render('recipes', { recipes: recipes })
 })
-
 router.get('/recipes/:id', async (req, res) => {
-  const idNum = req.params
+  const idNum = req.params.id
   const recipe = await db.getRecipe(idNum)
-  console.log(idNum)
   res.render('recipe', { recipe: recipe })
 })
 
+//ingredient routes
+router.get('/ingredients', async (req, res) => {
+  const ingredients = await db.getIngredients()
+  res.render('ingredients', { ingredients: ingredients })
+})
+
+//shoppinglist route
+router.get('/shoppinglist', async (req, res) => {
+  res.render('shoppinglist')
+})
 module.exports = router
 
 //<img class="img-circle" src="{{image}}" alt="{{name}}">
